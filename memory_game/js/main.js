f49526fa74c2c41fae5c234635bb71f4092f7f4e
Tesiah Coleman <admin@Tesiahs-MacBook-Pro.local>
2019-04-24 01:05:41 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,16 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardInPlay.push(cardOne);
+cardInPlay.push(cardTwo);
+console.log("user flipped" + " " + cardInPlay);
+if (cardInPlay.length === 2) {
+	if (cardInPlay[0] === cardInPlay[1]) {
+		alert ("You found a match!");
+	}
+	else {
+		alert ("Sorry, try again");
+	}
+}
